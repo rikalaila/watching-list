1d04f2f841a5afe5aabfc899f90d4bb919675ddc
Rika Laila <rika.14117024@student.itera.ac.id>
2021-05-05 14:58:19 +0700

    Update Route

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ import Button from './Button';
 import Input from './Input';
 import Link from './Link';
 import Gap from './Gap';
-export * from './molecules';
 
 export { Button, Input, Link, Gap };
